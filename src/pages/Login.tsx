8f92adc1fe96b3123e8642c1d3457cd0b0c9ec18
nielmontesa <nirumontesa@gmail.com>
2025-06-08 19:16:35 +0800

    Set up auth pages.

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,3 +1,17 @@
+import { AiFillGoogleCircle } from "react-icons/ai";
+
 export const Login = () => {
-  return <div>Login</div>;
+  return (
+    <main className="w-screen h-screen flex justify-center items-center">
+      <div className="bg-neutral-800 w-fit p-24 rounded-lg border border-neutral-700">
+        <form className="flex flex-col justify-center items-center gap-3">
+          <h1 className="xl text-xl">Work-Out Logs App</h1>
+          <button className="bg-neutral-700 px-4 py-2 rounded-sm flex gap-2 items-center">
+            <AiFillGoogleCircle size={"22"} />
+            Login With Google
+          </button>
+        </form>
+      </div>
+    </main>
+  );
 };
